c8c1e603017760909993e703926101fe14c18741
LeeVaughn <dlvprogramming@gmail.com>
2020-09-05 20:09:07 -0700

    created initial file and added mapped out the planned functionality with comments

---
+++ game.rb
@@ -0,0 +1,21 @@
+# welcome player to the game
+# show the current high score if the player is playing again
+
+# generate a random number within a certain range
+
+# create a variable to track the number of guesses
+
+# prompt use to guess a number
+  # convert guess to a number
+  # inform the user if their input is a non-number
+
+# while guessed number does not equal random number
+  # let the player know if their guess is outside the number range
+  # display "It's lower" if the guess is too high
+  # display "It's higher" if the guess is too low
+  # increment the counter variable
+
+# Once the player has guessed the number inform the user that they "Got it"
+# Let the player know how many guesses it took
+# ask the player if they would like to play again
+# let them know the game is over
